1151d70608278b39aa2146fbf4e7db4960f3fb19
Sue Lim <sue.p.lim@gmail.com>
2025-11-28 21:11:48 +0800

    feat(homepage): full homepage creation

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./**/*.{html,js}",   // include all html/js files in project
+  ],
+  theme: {
+    extend: {
+      colors: {
+        rose: "#BF307F",
+        navy: "#0B0320",
+        deepPurple: "#2E266D",
+        midPurple: "#413B52",
+        blackish: "#333333",
+        white: "#FFFFFF",
+        black: "#000000",
+      },
+      screens: {
+        sm: "300px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+    },
+  },
+  plugins: [],
+};
