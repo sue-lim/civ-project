7198945bbed4d13415b8673f4bf61f5e513effea
Sue Lim <sue.p.lim@gmail.com>
2025-11-30 21:26:54 +0800

    feat(copyright): copyrightpage created with contents

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
 
-    // Example JS to update the status message if needed
-    const form = document.getElementById("contactForm");
-    const statusDiv = document.getElementById("statusMessage");
+// JS to update the status message if needed
+const form = document.getElementById("contactForm");
+const statusDiv = document.getElementById("statusMessage");
 
-    form.addEventListener("submit", function (e) {
-        e.preventDefault();
-        statusDiv.textContent = "sending...";
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    statusDiv.textContent = "sending...";
 
-        // Simulate async send
-        setTimeout(() => {
-            statusDiv.textContent = "Message sent!";
-        }, 1500);
-    });
+    // Simulate async send
+    setTimeout(() => {
+        statusDiv.textContent = "Message sent!";
+    }, 1500);
+});
 
-    //Slider 
-    function moveToSlide(index) {
-        document.getElementById("carousel-inner").style.transform =
-          `translateX(-${index * 100}%)`;
-      }
+//Slider 
+function moveToSlide(index) {
+    document.getElementById("carousel-inner").style.transform =
+        `translateX(-${index * 100}%)`;
+}
